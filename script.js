9448917ef398853bb8baff8c371b8abec559b98c
Daniel <leinad6ix.z@gmail.com>
2022-08-14 13:05:22 -0700

    Finished password generator

---
+++ script.js
@@ -0,0 +1,49 @@
+// This is what the computer will choose from.
+const Pass = {
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    number: "0123456789",
+    symbol: "!@#$%^&*()-_=+[{}]|<>?/",
+}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+    var passwordSet = "";
+    
+    var length = prompt("How long would you like your password to be from 8-128")
+        console.log(length)
+
+    var upperCase = confirm("Would you like to add uppercase letters?");
+    if (upperCase) {
+        passwordSet += Pass.upperCase;
+    };
+    var lowerCase = confirm("Would you like to add lowercase letters?");
+    if (lowerCase) {
+        passwordSet += Pass.lowerCase
+    }
+    var number = confirm("Would you like to add numbers?");
+    if(number) {
+        passwordSet += Pass.number
+    }
+    var symbol = confirm("Would you like to add symbols?");
+    if (symbol) {
+        passwordSet += Pass.symbol
+    }
+
+    var password = "";
+    for (let i = 0; i < length; i++) {
+        password += passwordSet[Math.floor(Math.random() * passwordSet.length)]
+    }
+    return password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
